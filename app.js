b422d22e4c8fc59c18e65012cf531e86ed997f2a
sharmag44 <sharmagshambhu44@gmail.com>
2021-02-12 21:57:59 +0530

    Lots of changes maded or switch case added

--- app.js
+++ app.js
@@ -1,17 +1,41 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 const app = express();
-
+app.set('view engine','ejs');
 app.get("/",function(req,res)
 {
-     var today = new Date();
-     if (today.getDay()=== 6 || today.getDay()===0)
-    res.send("Todo List");
-});
+            var today = new Date();
+            var currentDay = today.getDay();
+     
+            var day = "";
+           switch(currentDay){
+             case 1:
+                 day ="Monday";
+                 break;
+              case 2:
+                    day ="Tuesday";
+                    break;
+            case 3:
+                day ="Wednesday";
+                 break;
+                 case 4:
+                    day ="Thursday";
+                    break;
+                    case 5:
+                        day ="Friday";
+                        break;
+                    case 6:
+                        day="Saturday";
+                        break;
+                    default:
+                       console.log("Error: Current day is not available${currentDay}");
+           }
+            res.render('list',{TypeofDay:day});
+}       );
 
-app.listen(PORT, ()=>
+app.listen(port, ()=>
 {
     console.log("Server is started at port 3000");
 });
