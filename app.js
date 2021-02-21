bc10d6625f32fb071d13a05f8e1b437f3453b17f
sharmag44 <sharmagshambhu44@gmail.com>
2021-02-21 22:40:03 +0530

    add todo list added

--- app.js
+++ app.js
@@ -1,41 +1,33 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const port = process.env.PORT || 3000;
-
 const app = express();
+
+app.use(bodyParser.urlencoded({extended:true}));
+ var items = [];
 app.set('view engine','ejs');
 app.get("/",function(req,res)
 {
             var today = new Date();
-            var currentDay = today.getDay();
-     
-            var day = "";
-           switch(currentDay){
-             case 1:
-                 day ="Monday";
-                 break;
-              case 2:
-                    day ="Tuesday";
-                    break;
-            case 3:
-                day ="Wednesday";
-                 break;
-                 case 4:
-                    day ="Thursday";
-                    break;
-                    case 5:
-                        day ="Friday";
-                        break;
-                    case 6:
-                        day="Saturday";
-                        break;
-                    default:
-                       console.log("Error: Current day is not available${currentDay}");
-           }
-            res.render('list',{TypeofDay:day});
+            const  options = {
+                weekday: "long",
+                 day:"numeric",
+                 month : "long"
+            };
+
+            const day = today.toLocaleDateString("en-US",options);
+
+            res.render('list',{TypeofDay:day, Additem: items});
 }       );
 
+app.post("/",function(req,res)
+{
+    var task =req.body.item;
+      items.push(task);
+    res.redirect("/");
+})
 app.listen(port, ()=>
 {
     console.log("Server is started at port 3000");
 });
+
