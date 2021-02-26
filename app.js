b56dbb6da26b1a5260209b035835e5796c9cd8ae
sharmag44 <sharmagshambhu44@gmail.com>
2021-02-26 23:08:58 +0530

    Todo list almost completed

--- app.js
+++ app.js
@@ -3,31 +3,28 @@ const bodyParser = require("body-parser");
 const port = process.env.PORT || 3000;
 const app = express();
 
-app.use(bodyParser.urlencoded({extended:true}));
- var items = [];
-app.set('view engine','ejs');
-app.get("/",function(req,res)
-{
-            var today = new Date();
-            const  options = {
-                weekday: "long",
-                 day:"numeric",
-                 month : "long"
-            };
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+let items = ["Eat", "Read", "Sleep"];
+app.set("view engine", "ejs");
+app.get("/", function (req, res) {
+  let today = new Date();
+  const options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  };
 
-            const day = today.toLocaleDateString("en-US",options);
+  const day = today.toLocaleDateString("en-US", options);
 
-            res.render('list',{TypeofDay:day, Additem: items});
-}       );
-
-app.post("/",function(req,res)
-{
-    var task =req.body.item;
-      items.push(task);
-    res.redirect("/");
-})
-app.listen(port, ()=>
-{
-    console.log("Server is started at port 3000");
+  res.render("list", { TypeofDay: day, Additem: items });
 });
 
+app.post("/", function (req, res) {
+  var task = req.body.item;
+  items.push(task);
+  res.redirect("/");
+});
+app.listen(port, () => {
+  console.log("Server is started at port 3000");
+});
