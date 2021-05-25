567e71096a5fcd8897e873377d929aa76c7066d1
sharmag44 <sharmagshambhu44@gmail.com>
2021-05-25 19:53:20 +0530

    unnecessary like index.html is deleted

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const port = process.env.PORT || 3000;
+const ejs = require("ejs");
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
